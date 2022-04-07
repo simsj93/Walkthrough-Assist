489b6b8d7cdf78d699f7eede1e400632fd2aef5b
April Wyatt <aprilwyatttech@gmail.com>
2022-04-07 11:25:40 -0500

    new files

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import FrontPage from "./frontpage/FrontPage.jsx";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<FrontPage />} />
             </Routes>
         </BrowserRouter>
     )
