1c7cc67ba5d9ad350d7c84d302036859c571de14
ReidMcLain <reidmm@gmail.com>
2022-04-07 18:13:56 -0500

    added Leati's iFrame

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import FrontPage from "./frontpage/FrontPage.jsx";
-
-import VideoPlayer from "./VideoPlayer.jsx";
+import VideoPlayer from "./videopage/VideoPlayer.jsx";
+import Vid from "./videopage/iframe.jsx";
 
 
 const App = () => {
     return (
         <BrowserRouter>
-        
             <Routes>
                 <Route path="/" element={<FrontPage />} />
                 <Route path="/videos" element={<VideoPlayer />} />
+                <Route path="/vids" element={<Vid />} />
             </Routes>
         </BrowserRouter>
     )
--- client/frontpage/FrontPage.jsx
+++ client/frontpage/FrontPage.jsx
@@ -15,7 +15,6 @@ const FrontPage = () => {
         <About />
         <ContentCards />
         <Footer />
-
       </section>
     </>
   );
