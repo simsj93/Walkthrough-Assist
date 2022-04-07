4ff816358a6da18b01f5a114a3cfafab11b43d2e
ReidMcLain <reidmm@gmail.com>
2022-04-07 11:30:49 -0500

    added video player

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import VideoPlayer from "./pages/VideoPlayer.jsx";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/videos" element={<Videos />} />
-                <Route path="/test" element={<Test />} />
+                <Route path="/videos" element={<VideoPlayer />} />
             </Routes>
         </BrowserRouter>
     )
