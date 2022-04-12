b270fe22e3efa7b85ad30c4a0ae3d13137c81697
Jake Sims <jake.jake.sims@gmail.com>
2022-04-12 11:54:22 -0500

    incorporated new styles with latest updates

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx"
 import FrontPage from "./frontpage/FrontPage.jsx";
 import VideoPlayer from "./videopage/VideoPlayer.jsx";
 
